7fa3c0d28bac25b4deb74f08d1939d03b6fe743b
Andreia Lima <andreialima85@gmail.com>
2018-10-10 13:21:38 -0300

    Estado e Cidade

---
+++ src/main/java/com/alima/cursomc/repositories/EstadoRepository.java
@@ -0,0 +1,11 @@
+package com.alima.cursomc.repositories;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+import com.alima.cursomc.domain.Estado;
+
+@Repository
+public interface EstadoRepository extends JpaRepository<Estado, Integer> {
+
+}
